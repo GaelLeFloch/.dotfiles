979a0a94ca3c1aebaf51043da52f7afc5a63919e
GaelLeFloch <gael.lefloch24@gmail.com>
2024-05-10 17:11:07 +0200

    [i3] updating weather location

--- polybar/.config/polybar/scripts/weather.sh
+++ polybar/.config/polybar/scripts/weather.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -s wttr.in/Lyon?format=3 > /tmp/weather
+curl -s wttr.in/Rennes?format=3 > /tmp/weather
 if [ $(fgrep -c -i "redirect" /tmp/weather ) -gt 0 ]; then
 	echo Sign In to Network;
 elif [ $(fgrep -c -i "query|queries" /tmp/weather) -gt 0 ]; then
